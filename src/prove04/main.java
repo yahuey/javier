a86a0e4ba44f86b8bfc51415546041a0911a020a
Carol Mercau <mercau.carol@gmail.com>
2020-02-02 22:48:55 -0700

    Added a hello message.

--- src/prove04/main.java
+++ src/prove04/main.java
@@ -6,5 +6,7 @@ public class main {
     public static void main(String []args){
         System.out.println("Hello Javier");
         System.out.println("Hello Javier, this is Jordi!");
+        System.out.println("Hey Javier and Jordi, this is Carol!" +
+                "\nI *think* I was supposed to modify this repository!");
     }
 }
